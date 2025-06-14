ef94325610449b4690f701122b8e13eb0754dffe
zzuse <zzuse@outlook.com>
2025-06-14 03:38:09 -0500

    Imlementations

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Rust: The Complete Developer's Guide
 
 8. Adding Functionality to Structs -- commit 8edb47864f81fa413e34fccf4d8230013faf5cf7 
-10. Mutable vs Immutable Bindings
+10. Mutable vs Immutable Bindings -- commit 51e4d493310b2bc43ea449af0a04a2c568d5d2a1
+11. Implementations and Methods
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,28 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = vec!["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"]; // here is the fixed size array, not vector
+// inherent implementation
+impl Deck {
+    // the Self return type equal the parent type
+    // the new() is an associated function == class method
+    fn new() -> Self {
+        let suits = vec!["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"]; // here is the fixed size array, not vector
 
-    let mut cards = vec![]; // Variables are immutable by default
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        let mut cards = vec![]; // Variables are immutable by default
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
-    }
 
-    let deck = Deck { cards };
+        let deck = Deck { cards };
+        return deck;
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
     println!("Heres your deck: {:#?}", deck); //# easier to read
 }

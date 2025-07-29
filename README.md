f9633b2a4166d4dc8191b02349144c8ba79889b0
zzuse <zzuse@outlook.com>
2025-07-29 11:00:59 -0500

    Adding Impl to Enums

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ bank/
 37. Solution on Mutable Refs -- commit ee95d44987acc14eafc957b61d7b46ac485bc5df
 39. Basic of Lifetimes -- commit 23e570944087ccd92b7f59a134389855b523e21e
 42. Implementing Deposit and Withdraw -- commit cc48cb3c930abb04812be5025d9b9d916e786fc3
-43. Accounts and Bank Implementation
+43. Accounts and Bank Implementation -- commit 09de4bfb06bf034491206f4d2a0762e9f09f6901
 media/
-46. Defining Enums
+46. Defining Enums -- commit 2dcb07e3875288af5a306d2a27e603022355bfb0
+48. Adding Implementations to Enums
--- media/src/main.rs
+++ media/src/main.rs
@@ -5,6 +5,20 @@ enum Media {
     Audiobook { title: String },
 }
 
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book: {} {}", title, author)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie: {} {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook: {} ", title)
+        } else {
+            String::from("Media Description")
+        }
+    }
+}
+
 fn print_media(media: Media) {
     println!("{:#?}", media);
 }
@@ -13,6 +27,20 @@ fn main() {
     let audiobook = Media::Audiobook {
         title: String::from("An Audiobook"),
     };
+    let good_movie = Media::Movie {
+        title: String::from("Good Movie"),
+        director: String::from("Good Director"),
+    };
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    println!("{}", audiobook.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
 
     print_media(audiobook);
+    print_media(good_movie);
+    print_media(bad_book);
 }

e0d463b86491921bced55cb68514e0e328c45129
zzuse <zzuse@outlook.com>
2025-10-15 15:00:51 -0500

    super solve

--- README.md
+++ README.md
@@ -56,4 +56,5 @@ lifetimes/
 108. Common Lifetimes -- commit 6836a761a5c4aa6841b35e5a57e8dc79bc1361ab
 generics/
 111. num_traits -- commit a54acee161a6d6206fee94a3e25da7fc0b725020
-113. Multiple Generic Types
+113. Multiple Generic Types -- commit f3ed6e9e535ba620719e733ea9e9478f48a89c4c
+114. Super solve
--- generics/src/main.rs
+++ generics/src/main.rs
@@ -17,9 +17,16 @@ fn solve_diff<T: Float, U: Float>(a: T, b: U) -> f64 {
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
+fn solve_primitive<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
 fn main() {
     let a: f32 = 3.0;
     let b: f32 = 4.0;
+    let c: u8 = 4;
 
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b as f64;
@@ -28,4 +35,5 @@ fn main() {
     println!("solve_both {}", solve_both::<f32>(a, b));
     println!("solve_both {}", solve_both::<f64>(a_f64, b_f64));
     println!("solve_diff {}", solve_diff(a, b_f64));
+    println!("solve_primitive {}", solve_primitive(a, c));
 }
